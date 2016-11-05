9e92f28accffc9e9f70dc112d312286937a9a6e1
Zhentar <Zhentar@gmail.com>
2016-11-05 18:44:17 -0500

    Add project files.

---
+++ Source/ScarringFix.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace ZhentarFix
+{
+	class ScarringFix : Hediff_Injury
+	{
+		public void DirectHeal(float amount) 
+		{
+			if (amount <= 0f)
+			{
+				return;
+			}
+			if (this.FullyHealableOnlyByTend() && this.Severity - amount <= 2f)
+			{
+				amount = this.Severity - 2f;
+			}
+			if (amount <= 0f)
+			{
+				return;
+			}
+			this.Severity -= amount;
+			if (this.comps != null)
+			{
+				for (int i = 0; i < this.comps.Count; i++)
+				{
+					this.comps[i].CompPostDirectHeal(amount);
+				}
+			}
+		}
+	}
+}
---
+++ Source/SmeltingFix.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace ZhentarFix
+{
+	class SmeltingFix
+	{
+		public bool Matches(Thing t)
+		{
+			return t.def.thingCategories.Intersect(ThingCategoryDefOf.Weapons.ThisAndChildCategoryDefs).Any() && !t.Smeltable;
+		}
+
+		public bool AlwaysMatches(ThingDef def)
+		{
+			return def.thingCategories.Intersect(ThingCategoryDefOf.Weapons.ThisAndChildCategoryDefs).Any() && !def.MadeFromStuff && def.smeltProducts == null;
+		}
+	}
+}

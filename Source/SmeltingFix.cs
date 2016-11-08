779452c3f285de294cbbbec5a70758da158e9284
Zhentar <Zhentar@gmail.com>
2016-11-08 08:48:04 -0600

    Allow smelting guns

--- Source/SmeltingFix.cs
+++ Source/SmeltingFix.cs
@@ -11,12 +11,23 @@ namespace ZhentarFix
 	{
 		public bool Matches(Thing t)
 		{
-			return t.def.thingCategories.Intersect(ThingCategoryDefOf.Weapons.ThisAndChildCategoryDefs).Any() && !t.Smeltable;
+			return t.def.thingCategories.Intersect(ThingCategoryDefOf.Weapons.ThisAndChildCategoryDefs).Any() && !IsSmeltable(t);
 		}
 
 		public bool AlwaysMatches(ThingDef def)
 		{
-			return def.thingCategories.Intersect(ThingCategoryDefOf.Weapons.ThisAndChildCategoryDefs).Any() && !def.MadeFromStuff && def.smeltProducts == null;
+			return def.thingCategories.Intersect(ThingCategoryDefOf.Weapons.ThisAndChildCategoryDefs).Any() && !def.MadeFromStuff && def.smeltProducts == null && !CostsSmeltable(def);
+		}
+
+		private bool CostsSmeltable(ThingDef def)
+		{
+			return (def.costList?.ElementAtOrDefault(0)?.thingDef.stuffProps?.smeltable).GetValueOrDefault();
+		}
+
+		private bool IsSmeltable(Thing t)
+		{
+
+			return !t.def.smeltProducts.NullOrEmpty() || (t.def.MadeFromStuff && t.def.stuffProps.smeltable) || CostsSmeltable(t.def);
 		}
 	}
 }

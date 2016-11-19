11882b1443082fd627bea445b2f1cb02ffe70c44
Zhentar <Zhentar@gmail.com>
2016-11-18 21:14:15 -0600

    Fixed bug with trading art text, inappropriate storyteller ramp down.

---
+++ Source/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ZhentarFix
+{
+	class Utils
+	{
+		public static Func<TValue> GetStaticFieldAccessor<TObject, TValue>(string fieldName)
+		{
+			var fieldInfo = typeof(TObject).GetField(fieldName, Detours.UniversalBindingFlags);
+			var member = Expression.Field(null, fieldInfo);
+			var lambda = Expression.Lambda(typeof(Func<TValue>), member);
+			var compiled = (Func<TValue>)lambda.Compile();
+			return compiled;
+		}
+
+		public static Func<TObject, TValue> GetFieldAccessor<TObject, TValue>(string fieldName)
+		{
+			var param = Expression.Parameter(typeof(TObject), "arg");
+			var member = Expression.Field(param, fieldName);
+			var lambda = Expression.Lambda(typeof(Func<TObject, TValue>), member, param);
+			var compiled = (Func<TObject, TValue>)lambda.Compile();
+			return compiled;
+		}
+	}
+}
